11d82093ec35da041bce862d2a484b3c21bab7a9
Elise <elisecloutier@hotmail.com>
2019-11-07 17:52:38 -0500

    unit tests

---
+++ app/src/test/java/com/example/seg2105walkinclinicservicesapp/UnitTest.java
@@ -0,0 +1,27 @@
+package com.example.seg2105walkinclinicservicesapp;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+
+public class UnitTest {
+    @Test
+
+    public void clinicID_isCorrect() {
+
+    }
+    public void loginUser_isCorrect() {
+
+    }
+    public void registerUser_isCorrect() {
+
+    }
+    public void setPatientSignup_isCorrect() {
+
+    }
+    public void setClinicSignup_isCorrect() {
+
+    }
+
+}
