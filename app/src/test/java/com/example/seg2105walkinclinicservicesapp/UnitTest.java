4a1c3774532eabf86c104861eb876459efcf8e74
Skyla Thadani <skyla.thadani@gmail.com>
2019-11-22 23:54:19 -0500

    added unit tests

--- app/src/test/java/com/example/seg2105walkinclinicservicesapp/UnitTest.java
+++ app/src/test/java/com/example/seg2105walkinclinicservicesapp/UnitTest.java
@@ -1,8 +1,11 @@
 package com.example.seg2105walkinclinicservicesapp;
 
+import com.example.seg2105walkinclinicservicesapp.Timetable.Week;
+
 import org.junit.Test;
 
 import java.sql.Time;
+import java.time.LocalDate;
 
 import static org.junit.Assert.*;
 
@@ -28,8 +31,9 @@ public class UnitTest {
 
     public void areSlotsAvalible_isCorrect1(){
         Timetable t = new Timetable();
+        LocalDate date = LocalDate.now();
 
-        boolean actual = t.slotsOK(4,5);
+        boolean actual = t.areSlotsAvailable(date.minusDays(1),1,6);
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -38,7 +42,7 @@ public class UnitTest {
     public void areSlotsAvalible_isCorrect2(){
         Timetable t = new Timetable();
 
-        boolean actual = t.slotsOK(4,5);
+        boolean actual = t.areSlotsAvailable(LocalDate.now(),1,7);
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -47,7 +51,7 @@ public class UnitTest {
     public void areSlotsAvalible_isCorrect3(){
         Timetable t = new Timetable();
 
-        boolean actual = t.slotsOK(4,6);
+        boolean actual = t.areSlotsAvailable(LocalDate.now(),1,8);
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -56,7 +60,7 @@ public class UnitTest {
     public void areSlotsAvalible_isCorrect4(){
         Timetable t = new Timetable();
 
-        boolean actual = t.slotsOK(4,7);
+        boolean actual = t.areSlotsAvailable(LocalDate.now(),1,9);
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -65,7 +69,7 @@ public class UnitTest {
     public void areSlotsAvalible_isCorrect5(){
         Timetable t = new Timetable();
 
-        boolean actual = t.slotsOK(4,5);
+        boolean actual = t.areSlotsAvailable(LocalDate.now(),1,10);
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -73,7 +77,7 @@ public class UnitTest {
     @Test
     public void reserveSlots_isCorrect(){
         Timetable t = new Timetable();
-        boolean actual = t.rSlots(4,5,"Tester1");
+        boolean actual = t.reserveSlots(LocalDate.now(),1,6,"Tester1");
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -81,7 +85,7 @@ public class UnitTest {
     @Test
     public void reserveSlots_isCorrect2(){
         Timetable t = new Timetable();
-        boolean actual = t.rSlots(4,6,"Tester1");
+        boolean actual = t.reserveSlots(LocalDate.now(),1,7,"Tester1");
         boolean expected = true;
 
         assertEquals(actual,expected);
@@ -90,11 +94,34 @@ public class UnitTest {
     public void reserveSlots_isCorrect3(){
         Timetable t = new Timetable();
 
-        boolean actual = t.rSlots(4,7,"Tester1");
+        boolean actual = t.reserveSlots(LocalDate.now(),1,8,"Tester1");
         boolean expected = true;
 
         assertEquals(actual,expected);
     }
+
+    @Test
+    public void avalibleSlots(){
+        Timetable.Week t = new Timetable().new Week(5);
+
+        boolean actual = t.areSlotsAvailable(4,7,9);
+        boolean expected = true;
+
+        assertEquals(actual,expected);
+    }
+
+    @Test
+    public void avalibleSlots1(){
+        Timetable.Week t = new Timetable().new Week(5);
+
+        boolean actual = t.areSlotsAvailable(4,7,10);
+        boolean expected = true;
+
+        assertEquals(actual,expected);
+    }
+
+
+
 
 
 
